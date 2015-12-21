cb91f6371d391d0f4f2c88de0ee506ab16f5e46b
Vincent Davis <vincent@vincentdavis.net>
2015-12-21 15:27:27 -0600

    add test for eratosthenes

---
+++ seqs/tests/test_eratosthenes.py
@@ -0,0 +1,12 @@
+class test_Sieve(unittest.TestCase):
+    def test_Prime(self):
+        """Test that the first few primes are generated correctly."""
+        G = primes()
+        for p in [2,3,5,7,11,13,17,19,23,29,31,37]:
+            self.assertEqual(p,next(G))
+
+    def test_Practical(self):
+        """Test that the first few practical nos are generated correctly."""
+        G = PracticalNumbers()
+        for p in [1,2,4,6,8,12,16,18,20,24,28,30,32,36]:
+            self.assertEqual(p,next(G))
